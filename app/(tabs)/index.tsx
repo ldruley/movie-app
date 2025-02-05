2e6bb5919f1486e9f9c21706818ce5a426cb624e
JjustinnHo <jusho@csumb.edu>
2025-02-05 13:45:15 -0800

    Search Screen Layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,28 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, TextInput, View, Text, TouchableOpacity } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import React, {useState} from 'react';
 
 export default function HomeScreen() {
+  const genres: string[] = ['Action','Comedy','Drama', 'Fantasy', 'Horror', 'Romance', 'Sci.-Fi.', 'Thriller'];
+  const [showGenres, setGenres] = useState(false);
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+  const filterClick = () => {
+    setGenres(!showGenres);
+  }
+  const selectGenre = (genre: string) => {
+    setSelectedGenres((selected) => {
+      if (selected.includes(genre)) {
+        return selected.filter((item) => item !== genre);
+      } else {
+        return [...selected, genre];
+      }
+    });
+    
+  }
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -16,39 +33,28 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Search for a movie!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+
+      <ThemedView style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder='Search movie'
+          />
+          <TouchableOpacity onPress={filterClick} style={styles.filterBox}>
+            <Text style={styles.filterText}>Filter movies</Text>
+          </TouchableOpacity>
+          {showGenres && (
+            <View>
+              {genres.map(genre => (
+                <TouchableOpacity key={genre} onPress={() => selectGenre(genre)} style={styles.genreContainer}>
+                  <View style={[styles.checkBox, selectedGenres.includes(genre) && styles.checkedBox,]}/>
+                  <Text style={styles.genreText}>{genre}</Text>
+                </TouchableOpacity>
+              ))}
+            </View> 
+          )}
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -71,4 +77,42 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  searchContainer: {
+    alignItems: 'center',
+  },
+  searchInput: {
+    height: 40,
+    width: '100%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  filterBox: {
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  filterText: {
+    height: 'auto',
+    width: 'auto',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  genreContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 5,
+  },
+  genreText: {
+    fontSize: 18,
+  },
+  checkBox: {
+    width: 20,
+    height: 20,
+    borderWidth: 2
+  },
+  checkedBox: {
+    backgroundColor: '#ccc'
+  }
+  
 });
